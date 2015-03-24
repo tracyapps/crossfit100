190674c327d12e5915362a1ea63ddb110a2e641b
tracyapps <t@thisroad.org>
2015-03-24 11:59:14 -0500

    start formatting of single

--- single.php
+++ single.php
@@ -7,13 +7,19 @@
 
 get_header(); ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
-
-		<?php while ( have_posts() ) : the_post(); ?>
+<?php while ( have_posts() ) : the_post(); ?>
+	<div id="page" class="fullheight insidepage clearfix"
+	<?php if (has_post_thumbnail( $id, 'full' ) ){
+		$page_bgimage = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'full' ); ?>
+		style="background-image: url('<?php echo $page_bgimage[0]; ?>');"
+	<?php } ?>
+	>
+	<?php $page_bgoverlay = get_post_meta($post->ID, 'backgroundoverlay', true); ?>
+	<main id="main" class="overlaybg <?php echo $page_bgoverlay; ?> site-main fullheight" role="main">
 
 			<?php get_template_part( 'content', 'single' ); ?>
 
+		<div class="container darkOverlay">
 			<?php cf100_post_nav(); ?>
 
 			<?php
@@ -22,11 +28,9 @@ get_header(); ?>
 					comments_template();
 				endif;
 			?>
-
+		</div>
 		<?php endwhile; // end of the loop. ?>
 
-		</main><!-- #main -->
-	</div><!-- #primary -->
-
-<?php get_sidebar(); ?>
+	</main><!--/main -->
+</div><!-- /page -->
 <?php get_footer(); ?>
