b71d15f33fcddaaf0a4c3aa0679af7f33cbc2381
tracyapps <t@thisroad.org>
2015-03-24 15:44:13 -0500

    fb audience pixel code

--- header.php
+++ header.php
@@ -46,7 +46,24 @@
 		} 
 	</style>
 <?php wp_head(); ?>
-
+	<script>
+		//fb audience pixel
+		(function() {
+			var _fbq = window._fbq || (window._fbq = []);
+			if (!_fbq.loaded) {
+				var fbds = document.createElement('script');
+				fbds.async = true;
+				fbds.src = '//connect.facebook.net/en_US/fbds.js';
+				var s = document.getElementsByTagName('script')[0];
+				s.parentNode.insertBefore(fbds, s);
+				_fbq.loaded = true;
+			}
+			_fbq.push(['addPixelId', '1591063397808993']);
+		})();
+		window._fbq = window._fbq || [];
+		window._fbq.push(['track', 'PixelInitialized', {}]);
+	</script>
+	<noscript><img height="1" width="1" alt="" style="display:none" src="https://www.facebook.com/tr?id=1591063397808993&amp;ev=PixelInitialized" /></noscript>
 </head>
 
 <body <?php body_class(); ?>>
